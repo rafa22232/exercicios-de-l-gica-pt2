56fe3371fcc0c6eb686f36c9b8733f282d210753
rafa22232 <rafael.ferreira347@etec.sp.gov.br>
2024-11-14 22:28:23 -0300

    Add files via upload

---
+++ ex1/js/ex1.js
@@ -0,0 +1,17 @@
+var n1 = parseFloat((prompt="Digite o primeiro número: "));
+var n2 = parseFloat((prompt="Digite o segundo número:"));
+
+if(n1>n2){
+
+
+
+alert("o maior número é:" + n1);
+}
+else if(n2>n1);
+
+alert("o maior número é:" + n2);
+  
+
+
+
+
